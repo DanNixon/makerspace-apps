e68c4bd25fc2b6d9f6f328a36a2c9ac5b6076446
Dan Nixon <dan@dan-nixon.com>
2025-04-15 18:02:06 +0100

    Traefik

---
+++ apps/traefik/render.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+{
+  echo "#"
+  echo "# This is a generated file!"
+  echo "#"
+  echo
+
+  helm template \
+    traefik \
+    traefik \
+    --repo https://helm.traefik.io/traefik \
+    --version "35.0.0" \
+    --namespace traefik \
+    --values src/values.yml
+
+  cat src/k8s/*.yml
+} > rendered.yml
