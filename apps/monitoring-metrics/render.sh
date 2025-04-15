a928b1e0de6326763d166444d9168ea9c6ab95c3
Dan Nixon <dan@dan-nixon.com>
2025-04-15 18:16:26 +0100

    Monitoring - metrics

---
+++ apps/monitoring-metrics/render.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+{
+  echo "#"
+  echo "# This is a generated file!"
+  echo "#"
+  echo
+
+  helm template \
+    prometheus \
+    prometheus \
+    --repo https://prometheus-community.github.io/helm-charts \
+    --version "27.8.0" \
+    --namespace monitoring-metrics \
+    --values src/values.yml \
+  | vals ksdecode -f -
+
+  cat src/k8s/*.yml
+} > rendered.yml
