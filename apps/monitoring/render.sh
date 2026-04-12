626e9a3f4395b1360ea90162541202a3d2b3b0ec
Dan Nixon <dan@dan-nixon.com>
2026-04-12 11:03:11 +0100

    Move to self hosted cloud Prometheus (#38)

--- apps/monitoring/render.sh
+++ apps/monitoring/render.sh
@@ -10,9 +10,10 @@
     k8s-monitoring \
     k8s-monitoring \
     --repo https://grafana.github.io/helm-charts \
-    --version "3.8.0" \
+    --version "4.0.1" \
     --namespace monitoring \
     --values src/values.yml \
+    --no-hooks \
   | vals ksdecode -f -
 
   cat src/k8s/*.yml
